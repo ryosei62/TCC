267c6c28bc0cd1df27d0e07e91fa0748fad2880e
koharu-614 <koharuuu8080@gmial.com>
2026-01-14 13:19:03 +0900

    VITEを付けた

--- api/cloudinary-delete.ts
+++ api/cloudinary-delete.ts
@@ -12,9 +12,9 @@ function initFirebaseAdmin() {
 
   admin.initializeApp({
     credential: admin.credential.cert({
-      projectId: process.env.FIREBASE_PROJECT_ID!,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
+      projectId: process.env.VITE_FIREBASE_PROJECT_ID!,
+      clientEmail: process.env.VITE_FIREBASE_CLIENT_EMAIL!,
+      privateKey: process.env.VITE_FIREBASE_PRIVATE_KEY!.replace(/\\n/g, "\n"),
     }),
   });
 }
